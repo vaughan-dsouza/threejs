eb198af601d57f2526c78418526fd08136e45a09
Vaughan <vaughan.dsouza@kilowott.com>
2025-03-20 16:00:54 +0530

    all

---
+++ fireship/src/donut-and-grid.js
@@ -0,0 +1,50 @@
+import './style.css'
+
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  // canvas: document.getElementById('bg'),
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setY(10).setZ(30);
+
+renderer.render( scene, camera);
+
+const geometry = new THREE.TorusGeometry(10,3,50,100);
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff, 200);
+pointLight.position.set(10,10,10);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.05);
+
+scene.add(pointLight, ambientLight);
+
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate(){
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
